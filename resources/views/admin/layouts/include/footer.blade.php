e86a5c779f00cd39465f8d2ca914ce6af0bb40c1
kashifsindhu <kashifcodes@gmail.com>
2022-02-10 04:41:28 -0800

    first commit

--- resources/views/admin/layouts/include/footer.blade.php
+++ resources/views/admin/layouts/include/footer.blade.php
@@ -2,6 +2,6 @@
     <strong>Copyright &copy; {{date('Y')}} Milk Kitchen Limited</strong>
     All rights reserved.
     <div class=" d-none d-sm-inline-block float-right">
-        <b>Develop By LEADconcept</b>
+        <b>Develop By dumplogix</b>
     </div>
 </footer>
--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-md-6 text__center-main">
                 <div>
-                    <p class="m-0 ">© Copyright 2021 provitx . All rights reserved </p>
+                    <p class="m-0 ">© Copyright 2021 dumplogix . All rights reserved </p>
                 </div>
             </div>
             <div class="col-md-6 text-right text__center-main">
                 <div>
                     <p class="m-0">Designed & Developed by <a href="https://leadconcept.com/"
-                        target="_blank">LEADconcept</a>
+                        target="_blank">dumplogix</a>
                     </p>
                 </div>
             </div>
